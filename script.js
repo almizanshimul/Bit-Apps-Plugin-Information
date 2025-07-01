8f8ae2ea621bac56413ea78862c3990aedee0eed
Al Mizan <67356360+almizanshimul@users.noreply.github.com>
2025-07-01 11:06:32 +0600

    fetch data promis method

--- script.js
+++ script.js
@@ -7,48 +7,94 @@ let bitTotalActive = 0;
 let bitTotalDownload = 0;
 
 
-bitPlugins.forEach((plugin) => {
-    let url = `https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slug]=${plugin}&request[fields][icons]=true&request[fields][downloaded]=true`
-    fetch(url)
-        .then(response => response.json())
-        .then(json => {
-            let name = json.slug.replace("-", " ")
-            let img = json.icons["1x"]
-            let totalReview = json.num_ratings
-            let fiveStarReview = json.ratings[5]
-            let totalActive = json.active_installs
-            let totalDownload = json.downloaded
+// bitPlugins.forEach((plugin) => {
+//     console.log(plugin);
+//     let url = `https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slug]=${plugin}&request[fields][icons]=true&request[fields][downloaded]=true`
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(json => {
+//             let name = json.slug.replace("-", " ")
+//             let img = json.icons["1x"]
+//             let totalReview = json.num_ratings
+//             let fiveStarReview = json.ratings[5]
+//             let totalActive = json.active_installs
+//             let totalDownload = json.downloaded
 
-            bitTotalReview += totalReview
-            bitFiveStarReview += fiveStarReview
-            bitTotalActive += totalActive
-            bitTotalDownload += totalDownload
+//             bitTotalReview += totalReview
+//             bitFiveStarReview += fiveStarReview
+//             bitTotalActive += totalActive
+//             bitTotalDownload += totalDownload
 
-            let boxContent = ` <div class="box">
-            <img src=${img} alt=${name} width="80" height="80">
-                    <div class="text-content">
-                        <h2>${name}</h2>
-                        <p>Total Review: ${totalReview}</p>
-                        <p>5Star Review: ${fiveStarReview}</p>
-                        <p>Total Active: ${totalActive}</p>
-                        <p>Total Download: ${totalDownload}</p>
-                    </div>
-                    </div>
-                    `
-            cardParent.innerHTML += boxContent
-
-
-            document.getElementById("review").innerHTML = bitTotalReview
-            document.getElementById("fiveReview").innerHTML = bitFiveStarReview
-            document.getElementById("active").innerHTML = bitTotalActive
-            document.getElementById("download").innerHTML = bitTotalDownload
-            document.getElementById("shortActive").innerHTML = ", " + formatNumber(bitTotalActive)
-            document.getElementById("shortDownload").innerHTML = ", " + formatNumber(bitTotalDownload)
+//             let boxContent = ` <div class="box">
+//             <img src=${img} alt=${name} width="80" height="80">
+//                     <div class="text-content">
+//                         <h2>${name}</h2>
+//                         <p>Total Review: ${totalReview}</p>
+//                         <p>5Star Review: ${fiveStarReview}</p>
+//                         <p>Total Active: ${totalActive}</p>
+//                         <p>Total Download: ${totalDownload}</p>
+//                     </div>
+//                     </div>
+//                     `
+//             cardParent.innerHTML += boxContent
 
 
-        })
-})
+//             document.getElementById("review").innerHTML = bitTotalReview
+//             document.getElementById("fiveReview").innerHTML = bitFiveStarReview
+//             document.getElementById("active").innerHTML = bitTotalActive
+//             document.getElementById("download").innerHTML = bitTotalDownload
+//             document.getElementById("shortActive").innerHTML = ", " + formatNumber(bitTotalActive)
+//             document.getElementById("shortDownload").innerHTML = ", " + formatNumber(bitTotalDownload)
 
+
+//         })
+// })
+
+
+Promise.all(
+    bitPlugins.map(plugin => {
+        cardParent.innerHTML += `<div class="skeleton-box"></div>`;
+        let url = `https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&request[slug]=${plugin}&request[fields][icons]=true&request[fields][downloaded]=true`;
+        return fetch(url).then(res => res.json());
+    })
+).then(dataArray => {
+    cardParent.innerHTML = "";
+    dataArray.forEach(json => {
+        let name = json.slug.replace("-", " ");
+        let img = json.icons["1x"];
+        let totalReview = json.num_ratings;
+        let fiveStarReview = json.ratings[5];
+        let totalActive = json.active_installs;
+        let totalDownload = json.downloaded;
+
+        bitTotalReview += totalReview;
+        bitFiveStarReview += fiveStarReview;
+        bitTotalActive += totalActive;
+        bitTotalDownload += totalDownload;
+
+        let boxContent = `
+      <div class="box">
+        <img src=${img} alt=${name} width="80" height="80">
+        <div class="text-content">
+          <h2>${name}</h2>
+          <p>Total Review: ${totalReview}</p>
+          <p>5Star Review: ${fiveStarReview}</p>
+          <p>Total Active: ${totalActive}</p>
+          <p>Total Download: ${totalDownload}</p>
+        </div>
+      </div>
+    `;
+        cardParent.innerHTML += boxContent;
+    });
+
+    // Summary update
+    document.getElementById("review").innerHTML = bitTotalReview;
+    document.getElementById("fiveReview").innerHTML = bitFiveStarReview;
+    document.getElementById("active").innerHTML = bitTotalActive;
+    document.getElementById("download").innerHTML = bitTotalDownload;
+    document.getElementById("shortActive").innerHTML = ", " + formatNumber(bitTotalActive);
+    document.getElementById("shortDownload").innerHTML = ", " + formatNumber(bitTotalDownload);
+});
 
 function formatNumber(num) {
     if (num >= 1_000_000_000) {
